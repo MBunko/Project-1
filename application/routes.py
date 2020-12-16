7d9d9e2f47f0e656235afd97a1e63f8177525fa8
MBunko <micha@instance-1.europe-west2-c.c.avian-current-296216.internal>
2020-12-16 17:25:28 +0000

    added individual game page

--- application/routes.py
+++ application/routes.py
@@ -12,7 +12,7 @@ def add():
         if not form.validate_on_submit():
             return "Please supply both task name and status"
         else:
-            new_game = Games(Title=form.Title.data, Genre=form.Genre.data, Age_rating=form.Age_rating.data, Description= form.Description.data)
+            new_game = Games(Title=form.Title.data, Release_date=form.Release_date.data, Genre=form.Genre.data, Age_rating=form.Age_rating.data, Description= form.Description.data)
             db.session.add(new_game)
             db.session.commit()
             return redirect(url_for("home"))
@@ -24,3 +24,10 @@ def home():
     all_games = Games.query.all()
     
     return render_template("index.html", title="Home", all_games=all_games)
+
+
+@app.route('/game/<Title>', methods= ["GET", "POST"])
+def game(Title):
+    form=Add()
+    game = Games.query.filter_by(Title=Title).first()
+    return render_template("update.html", form=form, title=Title, game=game)
