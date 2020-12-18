325d3e97393782658f15a7b5421b6a8d18061740
Michael Bunko <73492032+MBunko@users.noreply.github.com>
2020-12-18 15:45:18 +0000

    remove -1 from startup.sh

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-sudo mkdir /opt/Project-1
-sudo chown -R jenkins /opt/Project-1
+sudo mkdir /opt/Project
+sudo chown -R jenkins /opt/Project
 sudo systemctl daemon-reload
 sudo systemctl stop Project-1.service
 sudo systemctl start Project-1.service
