b611b8262a4a37778de02d9fe1ce983005864673
Michael Bunko <73492032+MBunko@users.noreply.github.com>
2020-12-19 16:47:56 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,5 +7,5 @@ pip3 install -r requirements.txt
 sudo mkdir /opt/Project
 sudo chown -R jenkins /opt/Project
 sudo systemctl daemon-reload
-sudo systemctl stop Project-1.service
-sudo systemctl start Project-1.service
+sudo systemctl stop Project.service
+sudo systemctl start Project.service
